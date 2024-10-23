f35223bdc5d1f7f0ddf8134cd132f1e3362c606b
Israel Akpadja <drimmaculate147@gmail.com>
2024-10-23 14:37:56 +0000

    finished building rows

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import './App.css';
-import Row from './components/Row';
+import "./App.css";
+import Row from "./components/Row";
+import Banner from "./components/Banner";
 import requests from "./requests";
 
 function App() {
   return (
-    <div className='App'>
-      <h2>Hey let's build a netflix clone front end!</h2>
-      <Row title={"NETFLIX ORIGINALS"} fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title={"Trending Now"} fetchUrl={requests.fetchTrending}/>
-      <Row title={"Top Rated"} fetchUrl={requests.fetchTopRated}/>
-      <Row title={""} />
+    <div className="App">
+      <Banner />
+      <Row
+        title={"NETFLIX ORIGINALS"}
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title={"Trending Now"} fetchUrl={requests.fetchTrending} />
+      <Row title={"Top Rated"} fetchUrl={requests.fetchTopRated} />
+      <Row title={"Action Movies"} fetchUrl={requests.fetchActionMovies} />
+      <Row title={"Comedy Movies"} fetchUrl={requests.fetchComedyMovies} />
+      <Row title={"Horror Movies"} fetchUrl={requests.fetchHorrorMovies} />
+      <Row title={"Romance Movies"} fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title={"Documentaries"} fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
-
-
 
 export default App;
