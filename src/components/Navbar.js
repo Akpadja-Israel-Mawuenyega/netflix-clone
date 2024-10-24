f7103437702d0a9b0093c041cd89f79257cb318e
Israel Akpadja <drimmaculate147@gmail.com>
2024-10-24 15:17:17 +0000

    finished navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll", null);
+    };
+  }, []);
+
   return (
-    <div className="navbar">
+    <div className={`navbar ${show && "black_navbar"}`}>
       <img
         className="logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         alt="Netflix Logo"
+      />
+      <img
+        className="avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="user"
       />
     </div>
   );
